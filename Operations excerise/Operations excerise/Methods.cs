b81b35eab358e0f9ef35fae63757f77ade1e05b2
Zachariah Olenik <zjolenik139@gmail.com>
2022-08-09 18:03:12 -0400

    operations exercise

---
+++ Operations excerise/Operations excerise/Methods.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations_excerise
+{
+    internal class Methods
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static int Modulus(int x, int y)
+        {
+            return x % y;
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area;
+
+
+        }
+    }
+}
---
+++ Operations excerise/Operations excerise/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Operations_excerise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Add(5,4));
+            //Console.WriteLine(Sub(3,2));
+            //Console.WriteLine(Multiply(5,10));
+            //Console.WriteLine(Divide(8,4));
+            //Console.WriteLine(Modulus(4,2));
+            //Console.WriteLine("Hello World!");
+            
+            
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            if (a == 17 && b == 4)
+
+            
+           // Console.WriteLine($"{a} divided by {b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("Please enter the radius of your circle:");
+            var userInput = Console.ReadLine();
+            var radius = double.Parse(userInput);
+            Console.WriteLine(Methods.AreaOfCircle(radius));
+           
+        }
+
+         
+        
+        
+        
+        
+        
+        
+      
+
+
+
+
+    }
+}
